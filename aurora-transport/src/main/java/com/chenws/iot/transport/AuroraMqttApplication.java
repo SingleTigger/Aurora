d8aa5df082b604cc0e84378bab7319c40b53ac2a
chenws <single_tigger@163.com>
2019-10-08 12:05:36 +0800

    add

--- aurora-transport/src/main/java/com/chenws/iot/transport/AuroraMqttApplication.java
+++ aurora-transport/src/main/java/com/chenws/iot/transport/AuroraMqttApplication.java
@@ -3,6 +3,9 @@ package com.chenws.iot.transport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by chenws on 2019/8/31.
+ */
 @SpringBootApplication
 public class AuroraMqttApplication {
 
