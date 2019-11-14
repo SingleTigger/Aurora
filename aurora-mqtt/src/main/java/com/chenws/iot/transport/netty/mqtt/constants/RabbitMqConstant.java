1ca8a336622cd7a84efa3338b3a81304570158dc
chenws <single_tigger@163.com>
2019-11-14 10:24:51 +0800

    修改模块名称

---
+++ aurora-mqtt/src/main/java/com/chenws/iot/transport/netty/mqtt/constants/RabbitMqConstant.java
@@ -0,0 +1,11 @@
+package com.chenws.iot.transport.netty.mqtt.constants;
+
+/**
+ * Created by chenws on 2019/11/12.
+ */
+public interface RabbitMqConstant {
+
+    String IOT_EXCHANGE = "iot_exchange";
+
+    String IOT_MSG = "iot_message";
+}
